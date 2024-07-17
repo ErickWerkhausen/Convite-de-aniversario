415b164f83738e502a65dc57812209b75f880b87
ErickWerkhausen <142852118+ErickWerkhausen@users.noreply.github.com>
2024-07-16 21:33:32 -0300

    first commit

---
+++ src/scripts/modal.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.function-modal')
+
+function openModal() {
+    modal.classList.add('active')
+}
+function closeModal() {
+    modal.classList.remove('active')
+    
+}
